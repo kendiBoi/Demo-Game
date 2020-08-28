2f8b110527a5d6d834da8ac6c11b49eee23f1800
kendiBoi <49128854+kendiBoi@users.noreply.github.com>
2020-08-28 22:42:15 +0200

    Add enemy component and little map
    Enemy health, Enemy pathfinding, Enemy shoot

---
+++ New Demo/Assets/Scripts/projectile.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class projectile : MonoBehaviour
+{
+    public float speed;
+    private Transform player;
+    private Vector2 target;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+
+        target = new Vector2(player.position.x, player.position.y);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector2.MoveTowards(transform.position,target, speed*Time.deltaTime);
+        if(transform.position.x == target.x && transform.position.y == target.y)
+        {
+           DestroyProjectile();
+        }
+        FlipSprite();
+
+       
+    }
+    private void FlipSprite()
+    {
+        Vector3 spriteScale = transform.localScale;
+        if (transform.position.x < player.position.x)
+        {
+            spriteScale.x = 2;
+        }
+        if (transform.position.x < player.position.x)
+        {
+            spriteScale.x = -2;
+        }
+        transform.localScale = spriteScale;
+    }
+    void onTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("hit the player");
+            //Replace the space in <> with the appropriate method for player taking damage or losing health
+            //collision.GetComponent<Player or playerHealth>().TakeDamage(DamageAmount);
+
+        }
+    }
+
+    private void DestroyProjectile()
+    {
+        Destroy(gameObject);
+    }
+}
